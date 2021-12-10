592020fa45fbd3ee47e6379506f5ea619380bd7a
George Gu <minggu@amazon.com>
2021-12-10 14:49:38 +0800

    update the env

--- bin/py_tsp-workshop-infra.ts
+++ bin/py_tsp-workshop-infra.ts
@@ -5,10 +5,10 @@ import { PyTspWorkshopInfraStack } from '../lib/py_tsp-workshop-infra-stack';
 const app = new cdk.App();
 
 const vpcId = app.node.tryGetContext('vpcId');
-const env = vpcId ? {
-  account: process.env.CDK_DEFAULT_ACCOUNT,
-  region: process.env.CDK_DEFAULT_REGION,
-} : undefined;
+const env = {
+  account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT, 
+  region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION 
+};
 
 new PyTspWorkshopInfraStack(app, 'PyTspWorkshopInfraStack',
     {
